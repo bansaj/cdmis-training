bc74163ed0ca0c22a6d928d67535378ebc70dfdb
Kamal Lama <kamal09rumba@gmail.com>
2021-01-20 09:10:45 +0545

    Butwal Geojson

--- api/butwal.php
+++ api/butwal.php
@@ -5,5 +5,5 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
-$butwal_geojson = file_get_contents('butwal.json');
+$butwal_geojson = file_get_contents('butwalRFC.json');
 echo $butwal_geojson;
